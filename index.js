ad67f0dcf84f1935460de90624a591b44ea3553a
Brittany Nieves <neopetlover912002@yahoo.com>
2018-05-10 14:49:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
 }
 
 function deleteFromObjectByKey(obj, key){
-  var newObj = Object.assign({}, obj)
-  
- return Object.assign({}, obj, { [key]: value })
+  var newObj = Object.assign({}, obj);
+  delete newObj.key;
+ return newObj;
 }
