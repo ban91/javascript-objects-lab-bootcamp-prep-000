e7c034184ee406aa16c1526c1305dec8b9e6b1b7
Brittany Nieves <neopetlover912002@yahoo.com>
2018-05-10 13:54:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ dinner: "steak"}
 function updateObjectWithKeyAndValue(obj, key, value){
  return Object.assign({}, obj, { [key]: value })
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  obj[key] = value;
+  return obj
+}
